1279645a488ea8f77653ad2f763ba50eeae0024e
Ali Sirjani <alisirjani1234@gmail.com>
2023-07-08 22:18:20 +0330

    refactor: Update create_user method in UserManager refactor: Update create_superuser method in UserManager feat: Add phone number and username validation in create_user method feat: Add username validation in create_superuser method feat: Add extra_fields parameter in create_superuser method feat: set is_staff and is_superuser True in create_superuser method for user

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,29 +2,40 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserManager(BaseUserManager):
 
-    def create_user(self, phone, username=''):
+    def create_user(self, phone=None, username=None):
         """
         Creates and saves a User with the given username and password.
         """
+
+        if not username and not phone:
+            raise ValueError("You must provide either a username or a phone number.")
+
         user = self.model(
             username=username,
-            phone=phone,
+            phone_number=phone,
         )
 
         user.set_unusable_password()
         user.save()
         return user
 
-    def create_superuser(self, username, password=None):
+    def create_superuser(self, username, password=None, **extra_fields):
         """
         Creates and saves a superuser with the given username and password.
         """
-        user = self.create_user(
-            username,
+
+        if not username:
+            raise ValueError("You must provide either a username.")
+
+        user = self.model(
+            username=username,
         )
         user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
         user.is_admin = True
         user.save(using=self._db)
         return user
