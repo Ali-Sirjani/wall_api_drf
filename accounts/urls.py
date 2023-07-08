14ee56daa6e9aae7e4ea25e81b4eeca35399b0f4
Ali Sirjani <alisirjani1234@gmail.com>
2023-07-09 00:24:37 +0330

    refactor: Add url accounts app feat: Add url signup, login, check_code and logout

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'accounts'
+
+
+urlpatterns = [
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/check-code/', views.check_code_view, name='check_code'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+]
+
+
+
+
