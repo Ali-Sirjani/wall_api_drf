873cf9198a1702074734017a3944cb37f4dc29aa
Ali Sirjani <alisirjani1234@gmail.com>
2023-07-08 22:29:41 +0330

    feat: Add CustomUserAdmin for custom user administration refactor: Update fieldsets and add_form in CustomUserAdmin feat: Add save_form method to CustomUserAdmin for creation user based on custom manager feat: Add CustomUserAdmin for CodeVerify model administration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser, CodeVerify
+from .forms import CustomUserCreationAdminForm, CustomUserChangeForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', )}),
+    )
+
+    add_form = CustomUserCreationAdminForm
+    add_fieldsets = ((None, {'fields': ('username', 'phone_number', 'create_staff', 'password1', 'password2')}), )
+    list_display = ('username', 'phone_number', 'email', 'first_name',
+                    'last_name', 'is_staff')
+
+    def save_form(self, request, form, change):
+
+        if not change:
+            phone_number = form.cleaned_data['phone_number']
+            if phone_number:
+                print('this phone: ', phone_number)
+                username = phone_number.as_e164
+                print('this username: ', type(username))
+                form.instance = self.model.objects.create_user(
+                    phone=phone_number,
+                    username=username,
+                )
+            else:
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                form.instance = self.model.objects.create_superuser(
+                    username=username,
+                    password=password,
+                )
+        return super().save_form(request, form, change)
+
+
+@admin.register(CodeVerify)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+
