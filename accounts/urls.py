78db495b95cab10e3e604b61b202d84cbabf1dd2
Ali Sirjani <alisirjani1234@gmail.com>
2023-07-10 02:12:44 +0330

    feat: Add API endpoints for user login and code verification
    This commit adds new URL paths for the API endpoints related to user login and code verification.
    The paths '/login/api/' and '/login/check-code/api/' are added for the LoginAPI and CheckCodeAPI views, respectively.
    The 'name' attribute is set for each API endpoint.
    feat: Add API endpoint for token refresh
    
    This commit introduces a new URL path for the token refresh API endpoint.
    The path '/refresh/api/' is added for the TokenRefreshView from djangorestframework-simplejwt library.
    The 'name' attribute is set for the API endpoint.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from . import views
 
 app_name = 'accounts'
@@ -10,8 +12,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('login/check-code/', views.check_code_view, name='check_code'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/api/', views.LoginAPI.as_view(), name='token_obtain_pair'),
+    path('login/check-code/api/', views.CheckCodeAPI.as_view(), name='check_code_api'),
+    path('refresh/api/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-
-
-
