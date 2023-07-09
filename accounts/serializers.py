1e33df095f81a98cf57e307cbad87add31f3154b
Ali Sirjani <alisirjani1234@gmail.com>
2023-07-10 01:56:30 +0330

    feat: Add UserSerializer for user serialization
    This commit introduces the UserSerializer class for serializing CustomUser objects.
    It includes the username and phone_number fields.
    The get_username method is implemented to handle optional username serialization.
    feat: Add LoginSerializer for user login serialization
    
    This commit adds the LoginSerializer class for serializing login requests.
    It includes the phone_number field for phone number-based authentication.
    feat: Add CodeVarifySerializer for code verification serialization
    
    This commit introduces the CodeVarifySerializer class for serializing code verification requests.
    It includes the user_id and code fields.

---
+++ accounts/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from phonenumber_field.serializerfields import PhoneNumberField
+
+from .models import CustomUser, CodeVerify
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'phone_number')
+
+    def get_username(self, obj):
+        if obj.phone_number == obj.username:
+            return None
+        return obj.username
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    phone_number = PhoneNumberField(region='IR')
+
+    class Meta:
+        model = CustomUser
+        fields = ('phone_number', )
+
+
+class CodeVarifySerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = CodeVerify
+        fields = ('user_id', 'code')
