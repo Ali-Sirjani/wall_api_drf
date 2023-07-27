dde62bea0e5811ece99d5e2c167bd8dde32d24ce
Ali Sirjani <alisirjani1234@gmail.com>
2023-07-27 19:34:51 +0330

    refactor: Enhance 'AdsAdmin' in Django admin
    The 'AdsAdmin' class in the admin.py file has been enhanced to provide better control over the 'Ad' model in the Django admin interface.
    
    The following improvements have been made:
    
    Added 'expiration_date' and 'datetime_deleted' fields to 'readonly_fields' to prevent editing of these fields in the admin interface.
    
    Updated 'list_display' to display relevant fields such as 'title', 'price', 'active', 'confirmation', 'datetime_modified', 'expiration_date', and 'is_delete' in the admin list view.
    
    Specified sorting by 'datetime_modified' in descending order using 'ordering' to show the most recent modifications first.
    
    Added filters for 'is_delete' and 'active' fields in the admin list view to facilitate searching and categorization.
    
    Created a custom action named 'soft_delete_selected' to enable soft deletion for selected objects in the admin list view.
    
    Overridden the 'get_fields' method to dynamically adjust the fields based on whether the object has been soft-deleted or not.
    
    Customized the 'delete_model' method to implement soft deletion when deleting individual objects from the admin interface.
    
    Modified the 'get_actions' method to remove the default 'delete_selected' action, preventing accidental permanent deletions.
    
    With these enhancements, the admin interface now offers improved functionality for managing ads, including the ability to perform soft deletions, filtering, and sorting for better data management and user experience.

--- ads/admin.py
+++ ads/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 
 from .models import Category, Ad
 
@@ -10,6 +11,36 @@ class AdsAdmin(admin.ModelAdmin):
 
 @admin.register(Ad)
 class AdsAdmin(admin.ModelAdmin):
-    fields = ('author', 'title', 'text', 'price', 'image', 'status_product',
-              'category', 'location', 'phone', 'active', 'slug', 'confirmation', 'datetime_modified')
-    readonly_fields = ('author', 'datetime_modified')
+    readonly_fields = ('author', 'datetime_modified', 'expiration_date', 'datetime_deleted')
+    list_display = ('title', 'price', 'active', 'confirmation', 'datetime_modified', 'expiration_date', 'is_delete')
+    ordering = ('-datetime_modified', )
+    list_filter = ('is_delete', 'active')
+    actions = ('soft_delete_selected', )
+
+    def get_fields(self, request, obj=None):
+        fields = ['author', 'title', 'text', 'price', 'image', 'status_product', 'category',
+                  'location', 'phone', 'active', 'slug', 'confirmation', 'datetime_modified', 'expiration_date']
+
+        if obj.is_delete:
+            fields.extend(['is_delete', 'delete_with', 'datetime_deleted'])
+
+        return fields
+
+    def delete_model(self, request, obj):
+        if not obj.is_delete:
+            obj.soft_delete('staff')
+        else:
+            messages.info(request, 'Ad has was deleted')
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def soft_delete_selected(self, request, queryset):
+        # Perform the soft deletion for selected objects
+        for obj in queryset:
+            obj.soft_delete(reason='staff')
+
+    soft_delete_selected.short_description = 'Soft delete selected objects'
