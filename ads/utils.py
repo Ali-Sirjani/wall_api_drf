75a77ec84df3dd7d97b342bec256e34a60ee8617
Ali Sirjani <alisirjani1234@gmail.com>
2023-08-01 20:17:48 +0330

    refactor: Implement OTP reset mechanism
    This commit introduces a reset mechanism for the OTP (One-Time Password) in the 'phone_number_verification' function. The function now checks if the current time has exceeded the reset time, which is set to one minute after the initial code generation. If the reset condition is met, the 'code_varify' instance is reset, allowing users to request a new OTP and restart the verification process. This improvement ensures a smoother and more efficient user experience during phone number verification, enabling users to initiate the verification process anew if they return to the function after a considerable period of time.
    
    feat: Implement 'cancel_create' function
    
    This commit introduces a new function named 'cancel_create', designed to enable users to cancel the phone number verification process. When called with the 'cancel' parameter set to 'True', the function resets the 'code_varify' instance's 'code' attribute to 0, allowing users to restart the verification process if they wish. If the user did not request to create an ad yet, the function will return a 'fail' response with the message "You did not request to create an ad yet."
    
    Furthermore, if users repeatedly cancel the process, they will receive a 'max otp try' error from 'phone_number_verification' function, indicating that they have reached the maximum number of OTP attempts. In this case, the user must wait until the maximum OTP attempt period has elapsed before retrying the verification process.
    
    This function provides users with greater control and flexibility during the phone number verification process, enhancing the overall user experience. It ensures that users can cancel the verification process and retry it if needed while imposing limits to prevent abuse of the process.

--- ads/utils.py
+++ ads/utils.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -11,38 +13,62 @@ def phone_number_verification(request):
     ser = CodeVarifySerializer(data=request.data)
     if ser.is_valid():
         code_varify = user.codeverify
-        if code_varify.expiration_timestamp is not None:
-            send_again = ser.validated_data.get('send_again')
-            if send_again:
-                if code_varify.send_code():
-                    # send code to phone in request
-                    print('this is code: ', code_varify.code)
+        if code_varify.expiration_timestamp is not None and code_varify.code:
+            reset_time = code_varify.expiration_timestamp + timezone.timedelta(minutes=1)
+            if timezone.now() < reset_time:
+                send_again = ser.validated_data.get('send_again')
+                if send_again:
+                    if code_varify.send_code():
+                        # send code to phone in request
+                        print('this is code: ', code_varify.code)
 
-                    return Response({'send again': 'Done'}, status=status.HTTP_200_OK)
+                        return Response({'send again': 'Done'}, status=status.HTTP_200_OK)
 
-                else:
-                    return Response({'times': 'max otp try'}, status=status.HTTP_400_BAD_REQUEST)
+                    else:
+                        return Response({'times': 'max otp try'}, status=status.HTTP_400_BAD_REQUEST)
 
-            code = ser.validated_data.get('code')
+                code = ser.validated_data.get('code')
 
-            if code is None:
-                return Response({'code': 'is empty'}, status=status.HTTP_400_BAD_REQUEST)
+                if code is None:
+                    return Response({'code': 'is empty'}, status=status.HTTP_400_BAD_REQUEST)
 
-            if code_varify.code == code:
-                if not code_varify.is_expired():
-                    code_varify.reset()
-                    return True
+                if code_varify.code == code:
+                    if not code_varify.is_expired():
+                        code_varify.reset()
+                        return True
 
-                return Response({'code': 'code has expired'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response({'code': 'code has expired'}, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response({'code': 'wrong code'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'code': 'wrong code'}, status=status.HTTP_400_BAD_REQUEST)
+
+            else:
+                code_varify.reset()
 
         if code_varify.send_code():
             code_varify.create_code()
-
             # send code to phone in request
             print('this is code: ', code_varify.code)
 
-            return Response({'code': 'send'}, status=status.HTTP_200_OK)
+            return Response({'message': 'you must verification ad phone number', 'code': 'send'}, status=status.HTTP_200_OK)
+
+        return Response({'times': 'max otp try'}, status=status.HTTP_400_BAD_REQUEST)
 
     return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def cancel_create(request):
+    cancel = request.query_params.get('cancel')
+
+    if cancel == 'True':
+        code = request.user.codeverify.code
+        if code != 0:
+            request.user.codeverify.code = 0
+            request.user.codeverify.save()
+            return Response({'status': 'cancel'}, status=status.HTTP_200_OK)
+
+        return Response(
+            {'status': 'fail', 'message': 'You did not request for create a ad yet'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    return Response({'status': 'fail', 'message': 'send True for params cancel'}, status=status.HTTP_400_BAD_REQUEST)
