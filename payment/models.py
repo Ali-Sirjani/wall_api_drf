a7fca34188ccecdc99325e3743c2dcb20677e78a
Ali Sirjani <alisirjani1234@gmail.com>
2023-08-06 21:24:57 +0330

    feat: Add PackageAdToken model with soft delete
    This commit introduces the PackageAdToken model, which represents a package of advertisement tokens in the system. Each package has the following fields:
    
    name (CharField): Represents the unique name of the package.
    description (TextField): Stores the description of the package.
    price (PositiveIntegerField): Indicates the price of the package.
    discount (BooleanField): Tracks whether a discount is applicable to the package.
    discount_price (PositiveIntegerField):  Indicates the discount price of the package (optional). The help_text for this field displays the value of each ad token, retrieved from the settings.AD_TOKEN_PRICE configuration variable, to aid in convenient calculation of the discount price.
    token_quantity (PositiveIntegerField): Represents the quantity of tokens included in the package. This field has a unique constraint, allowing only one package with a specific token quantity. It also has a minimum value constraint of 1 (MinValueValidator(1)).
    created_by (ForeignKey): References the user who created the package.
    edited_by (ForeignKey): References the user who last edited the package.
    confirmed_by (ForeignKey): References the user who confirmed the package.
    unconfirmed_by (ForeignKey): References the user who unconfirmed the package.
    deleted_by (ForeignKey): References the user who deleted the package.
    undelete_by (ForeignKey): References the user responsible for restoring the package from soft delete.
    datetime_created (DateTimeField): Records the timestamp when the package was created.
    datetime_modified (DateTimeField): Stores the timestamp of the last modification made to the package.
    confirmation (BooleanField): Indicates whether the package requires confirmation by an admin.
    is_delete (BooleanField): Flags whether the package is marked as deleted.
    datetime_deleted (DateTimeField): Records the timestamp when the package was soft-deleted.
    
    In addition to these fields, the commit adds the soft delete functionality to the PackageAdToken model. The soft_delete method allows marking a package as deleted without physically removing it from the database. It sets the is_delete flag to True, records the user who deleted the package (deleted_by), and saves the deletion timestamp (datetime_deleted).
    
    To ensure data integrity, the name field has a unique constraint, enforcing uniqueness regardless of case sensitivity.
    
    User tracking is implemented through the created_by, edited_by, confirmed_by, unconfirmed_by, deleted_by, and undelete_by fields, which reference the user model (get_user_model()). These fields capture the users responsible for specific actions on the package.
    
    This new model enhances the system's advertisement token package management by providing unique constraints, soft delete functionality, and comprehensive user tracking capabilities.

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db.models.functions import Upper
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator
+from django.utils import timezone
 
-# Create your models here.
+
+class PackageAdToken(models.Model):
+    """
+    The PackageAdToken model is used to represent a package of advertisement tokens in the system.
+    Each package has a unique name and description, price, and quantity of tokens it offers.
+    Additionally, it tracks who created, edited, confirmed, and deleted the package, and when these actions took place.
+    """
+    name = models.CharField(max_length=200, unique=True,
+                            verbose_name=_('name'))
+    description = models.TextField(unique=True, verbose_name=_('description'))
+    price = models.PositiveIntegerField(verbose_name=_('price'))
+    discount = models.BooleanField(default=False, verbose_name=_('discount'))
+    discount_price = models.PositiveIntegerField(blank=True, null=True, verbose_name=_('discount price'),
+                                                 help_text=_(f'price of each ad token is {settings.AD_TOKEN_PRICE}'))
+    token_quantity = models.PositiveIntegerField(null=True, verbose_name=_('token quantity'), unique=True,
+                                                 validators=(MinValueValidator(1),))
+
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='packages_created',
+                                   null=True, verbose_name=_('created by'))
+    edited_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='packages_edited',
+                                  null=True, verbose_name=_('edited by'))
+    confirmed_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='packages_confirmed',
+                                     null=True, verbose_name=_('confirmed by'))
+    unconfirmed_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='packages_unconfirmed',
+                                       null=True, verbose_name=_('unconfirmed by'))
+    deleted_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='packages_deleted',
+                                   null=True, verbose_name=_('deleted by'))
+    undelete_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='packages_undeleted',
+                                    null=True, verbose_name=_('undelete by'))
+
+    datetime_created = models.DateTimeField(auto_now_add=True, null=True, verbose_name=_('datetime created'))
+    datetime_modified = models.DateTimeField(null=True, verbose_name=_('datetime modified'))
+
+    confirmation = models.BooleanField(default=False, verbose_name=_('confirmation'), help_text='this is for admin')
+
+    # soft-delete fields
+    is_delete = models.BooleanField(default=False, verbose_name=_('is delete'))
+    datetime_deleted = models.DateTimeField(null=True, blank=True, verbose_name=_('datetime deleted'))
+
+    class Meta:
+        # Constraint to ensure that package names are unique when case is ignored.
+        constraints = (
+            models.UniqueConstraint(Upper('name'), name='unique name PackageAdToken'),
+        )
+
+    def __str__(self):
+        return self.name
+
+    def soft_delete(self, user):
+        """
+        Marks the package as deleted without actually removing it from the database.
+        Also sets the user who deleted the package and the time of deletion.
+        """
+        self.is_delete = True
+        self.deleted_by = user
+        self.datetime_deleted = timezone.now()
+        self.save()
