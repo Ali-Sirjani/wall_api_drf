e284f55c850a7ac5ac5cfbe559aa78ae6a7b424e
Ali Sirjani <alisirjani1234@gmail.com>
2023-08-06 22:09:56 +0330

    feat: Add Order model for customer orders with associated package
    This commit introduces the Order model, which represents an order placed by a customer for a specific package. The model structure includes the following fields:
    
    customer (ForeignKey): References the customer who placed the order using Django's built-in get_user_model() function.
    package (ForeignKey): References the chosen package for the order, linking it to the PackageAdToken model.
    first_name (CharField): Stores the first name of the customer.
    last_name (CharField): Stores the last name of the customer.
    email (EmailField): Stores the email address of the customer.
    phone (PhoneNumberField): Stores the phone number of the customer.
    order_note (TextField): Allows the customer to provide additional notes for the order (optional).
    
    The following fields are filled based on the associated package information:
    price (PositiveIntegerField): Stores the price of the package.
    discount (BooleanField): Tracks whether a discount is applicable to the package.
    discount_price (PositiveIntegerField): Indicates the discount price of the package
    token_quantity (PositiveIntegerField): Stores the quantity of tokens included in the package.
    
    Additional fields include:
    completed (BooleanField): Tracks the completion status of the order.
    transaction (PositiveBigIntegerField): Generates a unique transaction ID for the order using the generate_short_uuid() function.
    datetime_ordered (DateTimeField): Records the date and time when the order was placed.
    datetime_paid (DateTimeField): Records the date and time when the order was paid.
    
    In addition, a new function generate_short_uuid() is implemented to generate a short unique identifier as an integer value using UUIDv4. This function is utilized to generate the transaction ID for each order. The generated transaction ID ensures uniqueness and serves as a concise identifier for each order.
    
    By incorporating the generate_short_uuid() function, the system can generate distinct transaction IDs for different orders, enhancing traceability and identification of orders within the system.
    
    This commit adds the Order model to facilitate the storage and management of customer orders, capturing relevant details such as customer information, package selection, pricing, and timestamps.

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -5,6 +7,8 @@ from django.db.models.functions import Upper
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator
 from django.utils import timezone
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class PackageAdToken(models.Model):
@@ -63,3 +67,56 @@ class PackageAdToken(models.Model):
         self.deleted_by = user
         self.datetime_deleted = timezone.now()
         self.save()
+
+
+def generate_short_uuid():
+    """Generates a short unique identifier as an integer value using UUIDv4."""
+    return str(uuid.uuid4().int)[:10]
+
+
+class Order(models.Model):
+    """
+       Represents an order placed by a customer for a specific package.
+
+       This model defines the structure for storing information about customer orders.
+       Each order is associated with a customer and a chosen package. It also captures
+       details such as order completion, transaction IDs, and timestamps.
+
+       Attributes:
+           customer (ForeignKey): A reference to the customer who placed the order.
+               It uses Django's built-in `get_user_model()` to reference the user model.
+
+           package (ForeignKey): A reference to the package chosen for the order.
+               This ForeignKey links the order to a specific package, allowing retrieval
+               of details such as price, discount, token quantity, and more.
+
+       Additional Notes:
+           - The `price`, `discount`, `discount_price`, and `token_quantity` fields
+             are filled out based on the information in the associated package.
+             This ensures accurate pricing and token quantity for the order.
+       """
+    customer = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='orders', null=True,
+                                 blank=True, verbose_name=_('customer'))
+    package = models.ForeignKey(PackageAdToken, on_delete=models.SET_NULL, related_name='orders', null=True,
+                                verbose_name=_('package'))
+    first_name = models.CharField(max_length=200, null=True, verbose_name=_('first name'))
+    last_name = models.CharField(max_length=200, null=True, verbose_name=_('last name'))
+    email = models.EmailField(max_length=254, null=True, blank=True, verbose_name=_('email'))
+    phone = PhoneNumberField(null=True, region='IR', verbose_name=_('phone'))
+    order_note = models.TextField(null=True, blank=True, verbose_name=_('order note'))
+
+    price = models.PositiveIntegerField(null=True, verbose_name=_('price'))
+    discount = models.BooleanField(default=False, verbose_name=_('discount'))
+    discount_price = models.PositiveIntegerField(blank=True, null=True, verbose_name=_('discount price'))
+    token_quantity = models.PositiveIntegerField(null=True, verbose_name=_('token quantity'),
+                                                 validators=(MinValueValidator(1),))
+
+    completed = models.BooleanField(default=False, blank=True, verbose_name=_('completed'))
+    transaction = models.PositiveBigIntegerField(default=generate_short_uuid, unique=True, editable=False,
+                                                 verbose_name=_('transaction'))
+
+    datetime_ordered = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime ordered'))
+    datetime_paid = models.DateTimeField(null=True, verbose_name=_('datetime payed'))
+
+    def __str__(self):
+        return f'transaction: {self.transaction}'
